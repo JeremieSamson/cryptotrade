8da3aeb912cec07d58d8f33c8d6c2db1960e9a0f
Jeremie Samson <jeremie.samson76@gmail.com>
2018-04-05 14:17:33 +0200

    Add remote IP

--- app/config/deploy/dev.rb
+++ app/config/deploy/dev.rb
@@ -1,4 +1,14 @@
+# Branch dev from git
 set :current_stage, "dev"
+
+# Directory on the server
 set :deploy_to, "/var/www/dev.cryptobox.tech"
+
+# Symfony env
 set :symfony_env_prod, "dev"
+
+# Remove --no-dev from composer options
 set :composer_options, '--no-interaction --optimize-autoloader --no-progress'
+
+# Keep app_dev.php
+set :clear_controllers, false
--- app/config/deploy/prod.rb
+++ app/config/deploy/prod.rb
@@ -1,5 +1,10 @@
+# Branch dev from git
 set :current_stage, "master"
+
+# Directory on the server
 set :deploy_to, "/var/www/cryptobox.tech"
+
+# Symfony env
 set :symfony_env_prod, "prod"
 
 
